c88bd3b9ca85756d791e912bbed6a2129386cf48
Saurav Pattnaik <sauravpattnaik2011@gmail.com>
2022-01-17 20:32:58 +0530

    Updated readme and folder structuring

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Weighted-Class-Based-Tfidf
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)[![ForTheBadgebuilt-with-love](http://ForTheBadge.com/images/badges/built-with-love.svg)](https://github.com/SauravPattnaikCS60/Weighted-Class-Based-Tfidf)
+
+## Inspiration behind WCBTFIDF
+
+Standard tfidf models select the features(number defined by the **max_features** param) using t**erm frequency** alone.
+This can create problems when the dataset is imbalanced resulting in words from the majority class being selected.
+As a result of this minority class gets under-represented in the matrix that is being returned by tfidf.
+
+
+## Solution
+
+To tackle this problem we break down the tfidf process class wise.
+Let us consider an example to understand what WCBTFIDF does under the hood
+
+Assume a dataset having two labels 0 and 1.
+**0** is present in **80%** of the records and **1** is present in **20%** of the records.
+
+If we run standard tfidf on this(with for example 300 features) it will pick the top 300 words by frequency
+from both the classes. There is a very high chance that words selected will be majorly from class 0 and we 
+might run the risk of under-representing class 1 severely.
+
+What wcbtfidf does is that first it calculates weight for each label.
+Weight here refers to how many features it should select from each class.
+
+Since class 0 is present in 80% of the records, wcbtfidf will pick 240 features from class 0 and 60 features
+from class 1.
+
+So essentially we run tfidf class wise on 0 and 1 labels with max features set as 240 and 60.
+
+After doing that, we combine the vocabulary from both these classes into a single list.It can be easily done since
+tfidf provides a **vocabulary_** param that stores the vocab.
+
+Finally this combined vocab is used as a fixed vocabulary in another tfidf model that is ran on the entire data.
+By fixing the vocab for the final tfidf we ensure that we are going to score on these set of words only.
+
+_To put it simply the 300 features choosen by wcbtfidf are a better representation of the overall data as compared
+to the features chosen by standard tfidf model._
+
+## RESULTS
+
+In the experiments conducted, wcbtfidf performed better than standard tfidf models. The results have been put
+into a notebook under the _demos_ folder.
+
---
+++ wcbtfidf.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+class Wcbtfidf:
+
+    def __init__(self, max_features):
+        self.max_features = max_features
+        self.combine_vocab = []
+        self.class_wise_vocab = {}
+        self.final_tfidf = None
+
+    def fit(self, X, y):
+
+        label_dict = y.value_counts(normalize=True).to_dict()
+        for key, val in label_dict.items():
+            new_val = int(np.round(val, 1) * self.max_features)
+            label_dict[key] = new_val
+
+        self.combine_vocab = self.return_total_vocab(X, y, label_dict)
+        self.final_tfidf = TfidfVectorizer(vocabulary=self.combine_vocab, stop_words='english')
+        self.final_tfidf.fit(X)
+
+    def transform(self, X):
+        transformed_data = self.final_tfidf.transform(X)
+        transformed_data = pd.DataFrame(transformed_data.toarray(), columns=self.combine_vocab)
+        return transformed_data
+
+    def return_total_vocab(self, X, y, label_dict):
+
+        exclude = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves",
+                   "you", "your", "yours", "yourself", "yourselves", "he", "him",
+                   "his", "himself", "she", "her", "hers", "herself", "it", "its",
+                   "itself", "they", "them", "their", "theirs", "themselves", "what",
+                   "which", "who", "whom", "this", "that", "these", "those", "am", "is",
+                   "are", "was", "were", "be", "been", "being", "have", "has", "had", "having",
+                   "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because",
+                   "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between",
+                   "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down",
+                   "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there",
+                   "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some",
+                   "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can",
+                   "will",
+                   "just", "don", "should", "now"]
+
+        total_vocab = []
+        for key, val in label_dict.items():
+            slice_data = X[y == key]
+            tfidf = TfidfVectorizer(max_features=val, stop_words=exclude)
+            tfidf.fit(slice_data)
+            vocab = list(tfidf.vocabulary_.keys())
+            total_vocab.extend(vocab)
+            exclude.extend(vocab)
+            self.class_wise_vocab[key] = vocab
+
+        return total_vocab
